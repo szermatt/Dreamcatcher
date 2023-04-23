917a4bae9bb2faf76840735deeaa5d82fdde97b4
Stephane Zermatten <szermatt@gmx.net>
2023-04-23 19:17:12 +0300

    Introduces a preferences screen, not hooked to anything.
    The preferences screen replaces the power off button.

---
+++ app/src/main/java/net/gmx/szermatt/dreamcatcher/Preferences.kt
@@ -0,0 +1,13 @@
+package net.gmx.szermatt.dreamcatcher
+
+import android.os.Bundle
+import android.util.Log
+import androidx.leanback.preference.LeanbackPreferenceFragment
+import net.gmx.szermatt.dreamcatcher.DreamCatcherApplication.Companion.TAG
+
+class DreamCatcherPreferenceFragment : LeanbackPreferenceFragment() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        Log.d(TAG, "Created prefs with root key $rootKey")
+        setPreferencesFromResource(R.xml.preference, rootKey)
+    }
+}
--- app/src/main/java/net/gmx/szermatt/dreamcatcher/TvActivity.kt
+++ app/src/main/java/net/gmx/szermatt/dreamcatcher/TvActivity.kt
@@ -3,46 +3,18 @@ package net.gmx.szermatt.dreamcatcher
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.ImageButton
-import android.widget.Toast
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
 import net.gmx.szermatt.dreamcatcher.DreamCatcherApplication.Companion.TAG
 
 /**
- * A placeholder activity for the application; its main functionality is in the service.
+ * The main activity, containing the settings.
  */
 class TvActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<ImageButton>(R.id.power_off)
-        button.setOnClickListener { _: View? ->
-            Toast.makeText(this, "Power Off Requested", Toast.LENGTH_LONG).show()
-            val op = WorkManager.getInstance(this).enqueue(
-                OneTimeWorkRequest.Builder(PowerOffWorker::class.java)
-                    .setConstraints(
-                        Constraints.Builder()
-                            .setRequiredNetworkType(NetworkType.CONNECTED)
-                            .build()
-                    )
-                    .build()
-            )
-            val result = op.result
-            result.addListener(
-                {
-                    try {
-                        result.get()
-                    } catch (e: Exception) {
-                        Toast.makeText(this, "Power Off Failed", Toast.LENGTH_LONG).show()
-                    }
-                },
-                mainExecutor,
-            )
-        }
+
+        val prefs = DreamCatcherPreferenceFragment()
+        fragmentManager.beginTransaction().replace(R.id.preferences, prefs).commit()
     }
 
     override fun onStart() {
